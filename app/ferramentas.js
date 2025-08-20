8b700179f1ac4eccd88ae38a401db0a4ddbd42b8
Nicolas Salgado <2000nsalgado@gmail.com>
2025-08-20 17:13:12 -0300

    feat: adiciona som ao trocar ferramenta

--- app/ferramentas.js
+++ app/ferramentas.js
@@ -4,6 +4,7 @@ document.getElementById("pa").onclick = function () {
   desativarFerramentas();
   usandoPa = true;
   document.body.style.cursor = "url(../graphical-assets/pa/pa.png), auto";
+  tocarSomFerramenta();
 };
 
 document.getElementById("balde").onclick = function () {
@@ -11,4 +12,10 @@ document.getElementById("balde").onclick = function () {
   usandoBalde = true;
   document.body.style.cursor =
     "url(../graphical-assets/balde/balde-de-agua.png), auto";
+  tocarSomFerramenta();
 };
+
+function tocarSomFerramenta() {
+  const som = new Audio("../audio/trocar-ferramenta.ogg");
+  som.play();
+}
